d076834500ef2b1dcd5bc93a7fa28254ea2ae3b6
Daniel Su <31903483+Awedtan@users.noreply.github.com>
2025-04-15 12:05:21 -0400

    update: cn-25-04-10-16-28-33_72615d (#96)
    * resize spine pngs to match atlas files
    
    * cn apk url
    
    * push arts/char_* separately
    
    * try single quotes
    
    * separate arts fr this time
    
    * quotesssssssss
    
    * fix git add dir
    
    * dont fail on add nothing
    
    * update: cn-25-04-10-16-28-33_72615d
    
    * Operator assets - arts/char_0
    
    * Operator assets - arts/char_1
    
    * Operator assets - arts/char_2
    
    * Operator assets - arts/char_3
    
    * Operator assets - arts/char_4
    
    * Operator assets - arts/char_5
    
    * Operator assets - arts/char_6
    
    * Operator assets
    
    * Enemy spine assets
    
    * Operator spine assets
    
    * Stage assets
    
    * Operator spine transparency
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ scripts/utils.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+extract_ab() {
+    local FILES=$1
+    local FOLDER=$2
+    local OPTIONS=$3
+
+    mkdir -p scripts/assets/$FOLDER scripts/extracted/$FOLDER
+    cp -f scripts/apk/assets/AB/Android/$FILES scripts/assets/$FOLDER || :
+    cp -f scripts/download/$FILES scripts/assets/$FOLDER || :
+    dotnet $ARKSTUDIOCLI scripts/assets/$FOLDER $OPTIONS -o scripts/extracted/$FOLDER
+}
+
+git_commit() {
+    local MODIFY=$1
+    local FOLDER=$2
+    local COMMIT_MSG=$3
+
+    rm -f /tmp/git_commit
+
+    if [[ "$MODIFY" == *"$FOLDER"* ]]; then
+        git add $FOLDER || :
+        if git diff --cached --quiet; then
+            echo "No changes to commit."
+            echo 0 | tee /tmp/git_commit
+        else
+            git commit -m "$COMMIT_MSG"
+            echo 1 | tee /tmp/git_commit
+        fi
+    else
+        git ls-files --others --exclude-standard $FOLDER -z | xargs -0 git add
+        if git diff --cached --quiet; then
+            echo "No changes to commit."
+            echo 0 | tee /tmp/git_commit
+        else
+            git commit -m "$COMMIT_MSG"
+            echo 1 | tee /tmp/git_commit
+        fi
+    fi
+}
